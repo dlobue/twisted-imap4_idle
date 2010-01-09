d5f3c2c8bf4dfb4264f30bdd1ecc57c465305384
Dominic LoBue <dominic.lobue@gmail.com>
2010-01-09 04:26:36 -0800

    more fixes and profiling stuff

--- testidle.py
+++ testidle.py
@@ -1,32 +1,30 @@
+from twisted.internet.ssl import ClientContextFactory
 from twisted.mail  import imap4
 from twisted.internet import protocol, defer
+from datetime import datetime
+
+import termstyle
 
 Command = imap4.Command
-
-class myCommand(imap4.Command):
-    def format(self, tag=None):
-        if tag is None:
-            return self.command
-        elif self.args is None:
-            return ' '.join((tag, self.command))
-        return ' '.join((tag, self.command, self.args))
 
 class IMAPFolderListProtocol(imap4.IMAP4Client):
 
     idle_folder = None
     _keepalive = None
 
+    '''
     def lineReceived(self, line):
-        if 'IDLE' in line or 'DONE' in line: print 'DEBUG lineReceived-\n\t%s' % line
+        if 'IDLE' in line or 'DONE' in line: print termstyle.red('DEBUG lineReceived-\n\t%s' % line)
         return imap4.IMAP4Client.lineReceived(self, line)
         #return super(IMAPFolderListProtocol, self).lineReceived(line)
         #return self.__super.lineReceived(line)
 
     def dispatchCommand(self, tag, rest):
-        print 'DEBUG dispatchCommand-\n\ttag: %s\n\trest: %s' % (tag, rest)
+        print termstyle.red('DEBUG dispatchCommand-\n\ttag: %s\n\trest: %s' % (tag, rest))
         return imap4.IMAP4Client.dispatchCommand(self, tag, rest)
         #return super(IMAPFolderListProtocol, self).dispatchCommand(tag, rest)
         #return self.__super.dispatchCommand(tag, rest)
+        '''
 
     def serverGreeting(self, capabilities):
         login = self.login(self.factory.username, self.factory.password)
@@ -36,7 +34,6 @@ class IMAPFolderListProtocol(imap4.IMAP4Client):
     def __loggedIn(self, results):
         self.state = 'AUTH'
         return self.IDLE('INBOX/MIEN/')
-        #return self.list("", "*").addCallback(self.__gotMailboxList)
 
     def IDLE(self, folder):
         self.idle_folder = folder
@@ -54,29 +51,15 @@ class IMAPFolderListProtocol(imap4.IMAP4Client):
         return d
 
     def response_IDLE(self, tag, rest):
-        print 'DEBUG response_IDLE'
-        print tag
-        print rest
+        print termstyle.blue('%s - DEBUG response_IDLE' % datetime.now())
+        print termstyle.blue('\ttag- %s' % tag)
+        print termstyle.blue('\trest - %s' % rest)
         return tag, rest
 
-    def sendCommand(self, cmd):
-        cmd.defer = defer.Deferred()
-        if self.waiting and cmd.command != 'DONE':
-            self.queued.append(cmd)
-            return cmd.defer
-        elif self.waiting and cmd.command == 'DONE':
-            #self.sendLine(cmd.format(self.waiting))
-            self.sendLine(cmd.command)
-            return cmd.defer
-        t = self.makeTag()
-        self.tags[t] = cmd
-        self.sendLine(cmd.format(t))
-        self.waiting = t
-        self._lastCmd = cmd
-        return cmd.defer
-
     def IDLEDone(self):
-        print 'running IDLEDone'
+        print termstyle.yellow('running IDLEDone')
+        self.state = self._prev_state
+        del self._prev_state
         cmd = 'DONE'
         cmd = Command(cmd)
         cmd.defer = defer.Deferred()
@@ -84,36 +67,32 @@ class IMAPFolderListProtocol(imap4.IMAP4Client):
         return cmd.defer
 
     def __cbterminateIDLE(self, *args, **kwargs):
-        print '__cbterminateIDLE'
-        print args
-        print kwargs
+        print termstyle.cyan('__cbterminateIDLE')
+        print termstyle.cyan(args)
+        print termstyle.cyan(kwargs)
         return self.logout()
 
     def __cbIDLE(self, rest):
-        print 'DEBUG- __cbIDLE\n\trest: %s' % rest
-        if 'accepted, awaiting DONE command' in rest:
+        print termstyle.green('DEBUG- __cbIDLE\n\trest: %s' % rest)
+        if self.state != 'IDLE' and 'accepted, awaiting DONE command' in rest or 'idling' in rest:
+            print termstyle.green('idle start conditions found')
             #print self.waiting
             #print self._lastCmd
             #print self.tags
+            self._prev_state = self.state
             self.state = 'IDLE'
             #self.keepAlive()
-            reactor.callLater( 10, self.IDLEDone)
+            reactor.callLater( 300, self.IDLEDone)
             return
             d = reactor.callLater( 30, self.IDLEDone)
             return d
         else:
-            return rest
+            print termstyle.green('idle continuation')
+            return self.response_IDLE('+', rest)
 
     def keepAlive(self):
         reactor.callLater(20, self.keepAlive)
         return self.noop()
-
-    def noop(self):
-        print 'noop!'
-        return imap4.IMAP4Client.noop(self)
-
-    def __gotMailboxList(self, list):
-        return [boxInfo[2] for boxInfo in list]
 
     def connectionLost(self, reason):
         if not self.factory.deferred.called:
@@ -129,9 +108,8 @@ class IMAPFolderListFactory(protocol.ClientFactory):
         self.deferred = defer.Deferred( )
 
     def stopFactory(self):
-        print 'stopping factory'
+        print termstyle.magenta(termstyle.bold('stopping factory'))
         return
-        return self.protocol.IDLEDone(protocol)
 
     def clientConnectionFailed(self, connection, reason):
         self.deferred.errback(reason)
@@ -139,6 +117,7 @@ class IMAPFolderListFactory(protocol.ClientFactory):
 if __name__ == "__main__":
     from twisted.internet import reactor
     import sys, getpass
+    termstyle.enable()
 
     def printMailboxList(*args, **kwargs):
         print 'DEBUG- printMailboxList'
@@ -166,4 +145,5 @@ if __name__ == "__main__":
         printMailboxList).addErrback(
         handleError)
     reactor.connectTCP(server, 143, factory)
+    #icon = reactor.connectSSL(server, 993, factory, ClientContextFactory())
     reactor.run( )
